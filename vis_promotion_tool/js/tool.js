a15d6bc3f33de6d0aec76b3f76c6eff8b2f6e943
haibaraa <belief_if@hotmail.com>
2018-04-16 12:15:46 +0800

    init

---
+++ vis_promotion_tool/js/tool.js
@@ -0,0 +1,29 @@
+﻿$(function(){
+    $('#btn').click(function(){
+        var originParam = $('#originParam').val();
+        var errorMsg = $('#errorMsg').val();
+        if (!originParam) {
+            alert("请输入“批量报名供应商SKU ID”!");
+            return;
+        }
+        if (!errorMsg) {
+            alert("请输入“页面提示错误”!");
+            return;
+        }
+
+        var paramList = originParam.split("\n");
+        var errorList = errorMsg.replace(/第三方接口查不到商品信息.供应商SKU ID:/, "").split(",");
+        var resultList = [];
+        for (var i = 0; i < paramList.length; i++) {
+            if($.inArray(paramList[i], errorList) === -1){
+                resultList.push(paramList[i]);
+            }
+        }
+
+        if (resultList.length === 0 ) {
+            alert("没有可用的供应商SKU ID！");
+        } else {
+            $('#result').val(resultList.join("\n"));
+        }
+    });
+})
